62d0ec4aa2ace855c9e0dd1ca81a53ecc5ef35ba
Zachary Flota <zflota1@gmail.com>
2022-08-08 20:08:52 -0400

    feat: install mongoose and nodemon, declare mongoose schema for user

--- client/src/components/create-user.component.js
+++ client/src/components/create-user.component.js
@@ -3,8 +3,20 @@ import React, { Component } from 'react';
 export default class CreateUser extends Component {
     render() {
         return (
-            <div>
-                <p>Create User Component!!</p>
+            <div className="wrapper">
+                <form>
+                    <div className="form-group">
+                        <label>Enter Name</label>
+                        <input type="text" className="form-control" />
+                    </div>
+                    <div className="form-group">
+                        <label>Enter Email</label>
+                        <input type="text" className="form-control" />
+                    </div>
+                    <div className="form-group">
+                        <input type="submit" value="Create User" className="btn btn-success btn-block" />
+                    </div>
+                </form>
             </div>
         )
     }
